865e6fe905dce2ff9f83b64ca7cffd0f1bce69e1
backend <shubhamprabhu1001@gmail.com>
2024-11-10 11:41:13 +0530

    admin auth created

--- server/models/admin.model.js
+++ server/models/admin.model.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt")
 
 const adminSchema = new mongoose.Schema({
   adminName: {
@@ -25,4 +26,26 @@ const adminSchema = new mongoose.Schema({
   },
 });
 
+
+adminSchema.pre("save", async function (next) {
+  if(!this.isModified("password")) return next();
+
+  this.password = await bcrypt.hash(this.password, 10)
+  next()
+})
+
+adminSchema.methods.isPasswordCorrect = async function(password){
+  return await bcrypt.compare(password, this.password)
+}
+adminSchema.methods.generateAccessToken = function(){
+  return jwt.sign(
+      {
+          _id: this._id,
+      },
+      process.env.ACCESS_TOKEN_SECRET,
+      {
+          expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+      }
+  )
+}
 module.exports = mongoose.model("Admin", adminSchema);
